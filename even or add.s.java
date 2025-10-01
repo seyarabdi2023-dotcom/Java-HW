85257ea7a3b980ad3b587647f64f2b6e7e4f6473
seyarabdi2023-dotcom <seyarabdi2019@gmail.com>
2025-10-01 19:52:04 +0430

    Add files via upload

---
+++ even or add.s.java
@@ -0,0 +1,24 @@
+package hw;
+import java.util.Scanner;
+public class evenoradd {
+/**
+ *
+ * @author Seyar Abdi
+ */
+
+    public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+
+System.out.print("Even and Odd: ");
+        System.out.println("");            
+        int n = sc.nextInt();
+   if (n % 2 == 0) {
+   System.out.println(n + " is Even");
+   else {
+   System.out.println(" is Odd");}
+}
+
+
+      
+
+}
